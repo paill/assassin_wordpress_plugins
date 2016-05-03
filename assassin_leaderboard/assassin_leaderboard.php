8cde245cd182b08cc3835276c04be3a0cfefc81e
Paul Salessi <pasalessi@ucdavis.edu>
2016-05-03 06:16:06 +0100

    Added dataTable file includes to Wordpress page header

--- assassin_leaderboard/assassin_leaderboard.php
+++ assassin_leaderboard/assassin_leaderboard.php
@@ -5,7 +5,15 @@ Description: List all players with shortcode [assassin-leaderboard]
 Author:		 Paul Salessi
 */
 
-// 1. Add assassin-leaderboard shortcode
+// 1. Add DataTables script tags to page header
+add_action('wp_head','include_datatables');
+function include_datatables() {
+	$datatables = '<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.11/css/jquery.dataTables.css">';
+	$datatables .= '<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.11/js/jquery.dataTables.js"></script>';
+	echo $datatables;
+}
+
+// 2. Add assassin-leaderboard shortcode and construct HTML user table
 // Going to use DataTables for this
 function assassin_leaderboard( $atts ){
 	$blogusers = get_users();
@@ -20,3 +28,5 @@ function assassin_leaderboard( $atts ){
 	return $blogusers_content;
 }
 add_shortcode( 'assassin-leaderboard', 'assassin_leaderboard' );
+
+// 3. Create DataTables Javascript object
