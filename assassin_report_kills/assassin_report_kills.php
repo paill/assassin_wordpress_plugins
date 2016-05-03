74416ef1799e3fd06c15d901465e21444b3a5c32
Paul Salessi <pasalessi@ucdavis.edu>
2016-05-03 11:05:10 +0100

    Kills are now logged in database.

--- assassin_report_kills/assassin_report_kills.php
+++ assassin_report_kills/assassin_report_kills.php
@@ -5,7 +5,28 @@ Description: Adds Dashboard page for submitting killcodes. A kill increases the 
 Author:		 Paul Salessi
 */
 
-// 1. Register new menu page for reporting kills	
+// 1. Create custom table for storing kill logs
+function assassin_kills_install() {
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . 'assassin_kill_logs';
+	
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		time_of_kill datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		who_died mediumint(9) NOT NULL,
+		killed_by mediumint(9) NOT NULL,
+		UNIQUE KEY id (id)
+	) $charset_collate;";
+	
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+}
+register_activation_hook( __FILE__, 'assassin_kills_install' );
+
+// 2. Register new menu page for reporting kills	
 function register_kill_report_menu_page() {
     add_menu_page(
         __( 'Report an Assassination', 'default' ),
@@ -18,8 +39,10 @@ function register_kill_report_menu_page() {
 } // end register_kill_report_menu_page
 add_action( 'admin_menu', 'register_kill_report_menu_page' );
 
+// 3. Create form for report kill codes and update on valid codes
 function report_kill_form_page() {
-
+	global $wpdb;
+	
     $hidden_field_name = 'hidden_kill';
 
     // See if the user has posted us some information
@@ -55,6 +78,18 @@ function report_kill_form_page() {
 			$new_killcode = substr( $new_killcode, 0, 8 );
 			update_user_meta( $killed_user->ID, 'killcode', $new_killcode);
 			
+			// Insert kill into log table
+			$table_name = $wpdb->prefix . 'assassin_kill_logs';
+
+			$wpdb->insert( 
+				$table_name, 
+				array( 
+					'time_of_kill' => current_time( 'mysql' ), 
+					'who_died' => $killed_user->ID, 
+					'killed_by' => $current_user->ID, 
+				) 
+			);
+			
 			?>
 			<div class="notice notice-success"><p><strong><?php _e('Successful assassination of ' . $killed_user->first_name . ' ' . $killed_user->last_name . '!', 'default' ); ?></strong></p></div>
 			<?php
@@ -88,3 +123,13 @@ function report_kill_form_page() {
 
 	<?php
 }
+
+// 4. Remove the table on plugin deactivation
+function assassin_kills_uninstall() {	
+	global $wpdb;
+	
+	$table_name = $wpdb->prefix . 'assassin_kill_logs';
+
+	$wpdb->query("DROP TABLE IF EXISTS $table_name");
+}
+register_deactivation_hook( __FILE__, 'assassin_kills_uninstall' );
