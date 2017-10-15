d5779d3199595794f0a597c3772618dc0991b1c4
Paul Salessi <paul.salessi@intel.com>
2017-10-14 20:25:57 -0700

    Add MIT License info

--- assassin_game_complete/install.php
+++ assassin_game_complete/install.php
@@ -4,6 +4,14 @@ Plugin Name: Assassin Game Complete Plugin
 Description: Combines the all the Assassin plugins for easier install. Included plugins: Assassin Custom User Profile, Assassin Kill Feed, Assassin Leaderboard, Assassin Login Page, Assassin Register Page, Assassin Report Kills, Facebook Share Meta
 Author: Paul Salessi
 Plugin URI: https://github.com/psalessi/assassin_wordpress_plugins
+
+Copyright 2017 Paul Salessi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 include dirname(__FILE__) . '/cup.php';
--- assassin_report_kills/assassin_report_kills.php
+++ assassin_report_kills/assassin_report_kills.php
@@ -3,6 +3,15 @@
 Plugin Name: Assassin Report Kills
 Description: Adds Dashboard page for submitting killcodes. A kill increases the submitters score and resets the killed user's killcode. Stores kills in a database table for logging purposes.
 Author:		 Paul Salessi
+Plugin URI: https://github.com/psalessi/assassin_wordpress_plugins
+
+Copyright 2017 Paul Salessi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 // 1. Create custom table for storing kill logs
